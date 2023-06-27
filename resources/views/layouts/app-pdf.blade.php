50f7acc0067beab165a36fbb9eed4fa17e7a5191
dimas <dimasfebrikuncoro@gmail.com>
2023-06-27 10:43:40 +0700

    membuat app-pdf blade

---
+++ resources/views/layouts/app-pdf.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<head>
+    <style>
+    .container {
+        margin-top: 20px;
+    }
+
+    .card {
+        margin-bottom: 20px;
+    }
+
+    .card-header {
+        background-color: #f2f2f2;
+        font-weight: bold;
+    }
+
+    .btn-primary {
+        background-color: blue;
+        color: white;
+    }
+
+    .table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    .table th,
+    .table td {
+        padding: 10px;
+        border: 1px solid #ccc;
+    }
+
+    .btn-success {
+        background-color: green;
+        color: white;
+    }
+
+    .alert-success {
+        background-color: #d4edda;
+        border-color: #c3e6cb;
+        color: #155724;
+        padding: .75rem 1.25rem;
+        margin-bottom: 1rem;
+        border-radius: .25rem;
+    }
+</style>
+</head>
+<body>
+            @yield('content')
+</body>
+</html>
