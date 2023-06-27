b283d428570eb4ef2d9f1bf71d4c1cbc4421d19a
dimas <dimasfebrikuncoro@gmail.com>
2023-06-27 09:09:37 +0700

    edit barang controller

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Barang;
 use Illuminate\Support\Facades\Storage;
+use Session;
 
 class BarangController extends Controller
 {
@@ -12,5 +13,65 @@ class BarangController extends Controller
     {
         $data = Barang::all();
         return view('barang', compact('data'));
+    }
+
+    public function simpan(Request $request)
+    {
+        $request->validate([
+            'nama_barang' => 'required|string',
+            'stok_barang' => 'required|number',
+            'harga_beli' => 'required|number',
+            'harga_jual' => 'required|number',
+        ]);
+
+        $file = $request->file('foto2');
+
+        $destination = public_path('images');
+
+        $file->move($destination, $file->getClientOriginalName());
+        
+        $data = new Barang();
+        $data->foto = $file->getClientOriginalName();
+        $data->nama_barang = $request->nama_barang;
+        $data->stok_barang = $request->stok_barang;
+        $data->harga_beli = $request->harga_beli;
+        $data->harga_jual = $request->harga_jual;
+
+        $data->save();
+        Session::flash('sukses', 'Data berhasil disimpan');
+        return back();
+    }
+
+    public function hapus($id)
+    {
+        $data = Barang::find($id);
+        $data->delete();
+        Session::flash('sukses', 'Data berhasil dihapus');
+        return back();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = Barang::find($id);
+
+        $fotoLawas = $data->foto;
+        $destination = public_path('images');
+        Storage::delete($destination."/".$fotoLawas);
+
+        if ($request->file('foto') !== null) {
+            $file = $request->file('foto');
+            
+            $file->move($destination, $file->getClientOriginalName());
+            $data->foto = $file->getClientOriginalName();
+        }
+
+        $data->nama_barang = $request->nama_barang;
+        $data->stok_barang = $request->stok_barang;
+        $data->harga_beli = $request->harga_beli;
+        $data->harga_jual = $request->harga_jual;
+
+        $data->save();
+        Session::flash('sukses', 'Data berhasil diupdate');
+        return back(); 
     }
 }
