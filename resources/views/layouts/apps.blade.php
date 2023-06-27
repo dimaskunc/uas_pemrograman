ec383f20e94d0e0916fd920b1676fcc6b4200864
dimas <dimasfebrikuncoro@gmail.com>
2023-06-27 09:39:30 +0700

    membuat apps blade baru

---
+++ resources/views/layouts/apps.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>UAS Pemrograman Web</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+        body {
+    font-family: Poppins!important;
+  }
+  
+    </style>
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+      
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
