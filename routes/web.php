4beb53cc390305f8aeb1b7cca9849baf2cec2589
dimas <dimasfebrikuncoro@gmail.com>
2023-06-27 09:02:27 +0700

    membuat barang route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/barang', [App\Http\Controllers\BarangController::class, 'index'])->name('barang');
+Route::post('/simpan-data-barang', [App\Http\Controllers\BarangController::class, 'simpan'])->name('simpan');
+Route::get('/hapus-data-barang/{id}', [App\Http\Controllers\BarangController::class, 'hapus'])->name('hapus');
+Route::post('/update-data-barang/{id}', [App\Http\Controllers\BarangController::class, 'update'])->name('update');
+
